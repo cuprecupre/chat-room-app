b10a5e0fedddc4e7dda01103bacec675e398567e
Leandro Vegas <leandrovegas@MacBook-Pro-Leandro.local>
2025-12-22 11:28:35 +0100

    chore: enable socket compression and enforce strict CI tests

--- apps/server/src/config/socketio.js
+++ apps/server/src/config/socketio.js
@@ -3,6 +3,9 @@ const socketIo = require("socket.io");
 function createSocketServer(httpServer) {
     return socketIo(httpServer, {
         cors: { origin: "*" },
+        perMessageDeflate: {
+            threshold: 1024, // Compress only if message > 1KB
+        }
     });
 }
 
