553b9016090b637cf9bb19ef302dca06bf639300
Mauro Barrionuevo <mauro@egg.live>
2026-01-01 21:19:32 -0300

    Default to websocket instead of polling

--- apps/server/src/config/socketio.js
+++ apps/server/src/config/socketio.js
@@ -3,6 +3,10 @@ const socketIo = require("socket.io");
 function createSocketServer(httpServer) {
     return socketIo(httpServer, {
         cors: { origin: "*" },
+        transports: ["websocket", "polling"], // Prioritize WebSocket, fallback to polling
+        pingTimeout: 60000, // 60s timeout (helps mobile connections)
+        pingInterval: 25000, // 25s ping interval
+        upgradeTimeout: 30000, // Give mobile more time to upgrade
     });
 }
 
